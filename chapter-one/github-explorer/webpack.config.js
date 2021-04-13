acff60e0f55f63bc09727bc5c24bb4837844a87c
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-04-12 22:56:32 -0300

    Configuração HtmlWebpackPlugin para servir html estático

--- chapter-one/github-explorer/webpack.config.js
+++ chapter-one/github-explorer/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPuugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -10,6 +11,11 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    plugins: [
+        new HtmlWebpackPuugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        }),
+    ],
     module: {
         rules: [
             {

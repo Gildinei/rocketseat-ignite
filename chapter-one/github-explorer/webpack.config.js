73b5ec5cca4fbe6887b8569520a96496e1b850f7
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-04-12 23:01:06 -0300

    Configuração do Webpack Dev Server

--- chapter-one/github-explorer/webpack.config.js
+++ chapter-one/github-explorer/webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    },
     plugins: [
         new HtmlWebpackPuugin({
             template: path.resolve(__dirname, 'public', 'index.html'),

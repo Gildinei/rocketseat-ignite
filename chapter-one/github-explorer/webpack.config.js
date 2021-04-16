d857104918d756943d3fc6b0991d096d16563a8f
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-04-16 10:14:48 -0300

    Adicionado sass-loader e node-sass para estilização

--- chapter-one/github-explorer/webpack.config.js
+++ chapter-one/github-explorer/webpack.config.js
@@ -32,11 +32,12 @@ module.exports = {
                 },
             },
             {
-                test: /\.css$/,
+                test: /\.scss$/,
                 exclude: /node_modules/,
                 use: [
                     { loader: 'style-loader' },
-                    { loader: 'css-loader' }
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' }
                 ],
             }
         ],

28234ec3bb98ad4c6abc7fc98db8a4bca3c33222
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-04-16 10:06:34 -0300

    Adicionado style-loader css-loader e configuração para arquivos css

--- chapter-one/github-explorer/src/App.jsx
+++ chapter-one/github-explorer/src/App.jsx
@@ -1,4 +1,6 @@
+import './styles/global.css'
+
 export function App() {
-    throw new Error("Algo de errado não está certo!");
+    // throw new Error("Algo de errado não está certo!");
     return <h1>Github Explorer</h1>
 };

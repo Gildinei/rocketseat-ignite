eaa4873b4174ac7fd410df8b482f69ef747839c6
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-04-17 10:36:52 -0300

    Adicionado RepositoryList

--- chapter-one/github-explorer/src/App.jsx
+++ chapter-one/github-explorer/src/App.jsx
@@ -1,6 +1,6 @@
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
-    // throw new Error("Algo de errado não está certo!");
-    return <h1>Github Explorer</h1>
+    return <RepositoryList />
 };
